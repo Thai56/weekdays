e96b29b30309b690ed50cc13edb2c48e0e96f0c0
Thai Tran <Thai56@Thais-MacBook-Air.local>
2017-02-06 22:11:07 -0700

    first commit

---
+++ index.ios.js
@@ -0,0 +1,27 @@
+//import some code we need
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
+//Create  a react component
+var Weekdays = React.createClass({
+  render: function() {
+    return <View style={styles.container}>
+              <Text>
+                Days of the week:
+              </Text>
+          </View>
+  }
+});
+
+//Style the React component
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+//Show the react component on the screen
+AppRegistry.registerComponent('weekdays', function() {
+  return Weekdays
+})
